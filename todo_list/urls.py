753996cbc69c7994ec73125ca75819af688c503c
Yaroslav Konoplyov <kannabis252@gmail.com>
2023-07-26 12:49:50 +0300

    added url for task create view

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 from todo_list.views import (TaskListView, TagListView, TagCreateView,
-                             TagUpdateView, TagDeleteView)
+                             TagUpdateView, TagDeleteView, TaskCreateView)
 
 urlpatterns = [
     path("", TaskListView.as_view(), name="task-list"),
+    path("task_create", TaskCreateView.as_view(), name="task-create"),
     path("tag_list", TagListView.as_view(), name="tag-list"),
     path("tag_list/create", TagCreateView.as_view(), name="tag-create"),
     path(
