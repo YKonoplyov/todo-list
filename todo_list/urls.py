b4cf9a131a85922f5fb9c8355738752b6e63e11f
Yaroslav Konoplyov <kannabis252@gmail.com>
2023-07-26 12:25:43 +0300

    url for tag list created

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path
+from todo_list.views import (TaskListView, TagListView)
 
 urlpatterns = [
-    path("", TaskListView.as_view(), name="task-list")
+    path("", TaskListView.as_view(), name="task-list"),
+    path("tag_list", TagListView.as_view(), name="tag-list"),
 ]
 
 app_name = "todo_list"
