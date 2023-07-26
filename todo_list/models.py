8079549727fdf22ea227eeee44cf3718ec0f8620
Yaroslav Konoplyov <kannabis252@gmail.com>
2023-07-26 12:08:27 +0300

    Task model created

--- todo_list/models.py
+++ todo_list/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
@@ -8,3 +9,17 @@ class User(AbstractUser):
 
 class Tag(models.Model):
     name = models.CharField(max_length=255)
+
+
+class Task(models.Model):
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    deadline = models.DateTimeField(null=True, blank=True)
+    is_completed = models.BooleanField(default=False)
+    tags = models.ManyToManyField(
+        Tag,
+        related_name="tasks",
+        null=True,
+        blank=True
+    )
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
