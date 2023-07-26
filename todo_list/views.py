4a07a2f35b38cefd754ba992aa89b75af273a0aa
Yaroslav Konoplyov <kannabis252@gmail.com>
2023-07-26 12:24:06 +0300

    created taglistview

--- todo_list/views.py
+++ todo_list/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.views import generic
-from todo_list.models import Task
+from todo_list.models import Task, Tag
 
 
 class TaskListView(generic.ListView):
     model = Task
+
+
+class TagListView(generic.ListView):
+    model = Tag
 
