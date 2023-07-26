2922c625eab9e544af98c1b5b1dbf2832bed5495
Yaroslav Konoplyov <kannabis252@gmail.com>
2023-07-26 12:48:58 +0300

    added task create view

--- todo_list/views.py
+++ todo_list/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
+from todo_list.forms import TaskForm
 from todo_list.models import Task, Tag
 
 
 class TaskListView(generic.ListView):
     model = Task
+
+
+class TaskCreateView(generic.CreateView):
+    model = Task
+    form_class = TaskForm
+    success_url = reverse_lazy("todo_list:task-list")
+
 
 
 class TagListView(generic.ListView):
