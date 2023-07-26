e10e9a27db6298d79343b2b391f942e6d48b61d0
Yaroslav Konoplyov <kannabis252@gmail.com>
2023-07-26 13:56:05 +0300

    added get absolut url method for user model

--- todo_list/models.py
+++ todo_list/models.py
@@ -1,10 +1,12 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.urls import reverse_lazy
 
 
 class User(AbstractUser):
-    pass
+    def get_absolute_url(self):
+        return reverse_lazy("todo_list:task-list")
 
 
 class Tag(models.Model):
