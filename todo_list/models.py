3f98f002d01f6693743fa0cc68fe598c513c95df
Yaroslav Konoplyov <kannabis252@gmail.com>
2023-07-26 12:05:02 +0300

    Tag model created

--- todo_list/models.py
+++ todo_list/models.py
@@ -1,6 +1,10 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 class User(AbstractUser):
     pass
 
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
