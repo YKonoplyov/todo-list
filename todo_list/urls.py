da0b71c6d2ebef6d152c2d6e037d2488902badff
Yaroslav Konoplyov <kannabis252@gmail.com>
2023-07-26 12:37:08 +0300

    added link to tag update

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -1,11 +1,17 @@
 from django.contrib import admin
 from django.urls import path
-from todo_list.views import (TaskListView, TagListView, TagCreateView)
+from todo_list.views import (TaskListView, TagListView, TagCreateView,
+                             TagUpdateView)
 
 urlpatterns = [
     path("", TaskListView.as_view(), name="task-list"),
     path("tag_list", TagListView.as_view(), name="tag-list"),
     path("tag_list/create", TagCreateView.as_view(), name="tag-create"),
+    path(
+        "tag_list/<int:pk>/update",
+        TagUpdateView.as_view(),
+        name="tag-update"
+    )
 ]
 
 app_name = "todo_list"
